5d5b63bd7802c4cf05b3a610f2aa6b5fa196f29a
ashwin-codex <ashwinkan2007@gmail.com>
2026-07-12 12:33:28 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (69.93%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int fib(int n) {
+        if (n == 0){
+        return 0;
+        }else if (n==1){
+            return 1;
+        }
+        int first_term =0;
+        int second_term =1;
+
+        for(int i=1 ; i<=n;i++){
+            int third_term = first_term + second_term;
+
+            first_term=second_term;
+            second_term=third_term;
+        }
+        return first_term;
+
+    }
+}
